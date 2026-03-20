98e3c66302ec7feb8fcbe1f8ecd7b65c0fc0005f
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2026-03-20 19:14:37 -0300

    Página de Login parcialmente concluída

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: const Color(0xFF195E3B)),
+        fontFamily: "MavenPro",
+        useMaterial3: true,
       ),
       home: const MyHomePage(),
     );
@@ -35,8 +37,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _alturaContainer = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        _alturaContainer = 500.0;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Column()));
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/logo.png"),
+                  SizedBox(width: 10),
+                  Text.rich(
+                    TextSpan(
+                      // Estilo base que vale para "Goa" e para "Link"
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      children: [
+                        const TextSpan(
+                          text: 'Goa',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        TextSpan(
+                          text: 'Link',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 800),
+              curve: Curves.easeOutQuart,
+              width: double.infinity,
+              height: _alturaContainer,
+              decoration: BoxDecoration(
+                color: Colors.black.withValues(alpha: 0.75),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: const SingleChildScrollView(
+                physics: NeverScrollableScrollPhysics(),
+                child: SizedBox(height: 500),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
